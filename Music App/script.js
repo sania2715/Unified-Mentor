41e3de8bcad1aa2956ff5bcc6737d336d13c2b18
Sania Nidagundi <146361748+sania2715@users.noreply.github.com>
2024-09-30 19:01:43 +0530

    Add files via upload

---
+++ Music App/script.js
@@ -0,0 +1,201 @@
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+let track_desc= document.querySelector(".track-desc");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+let wave = document.getElementById("wave");
+let randomIcon = document.querySelector(".fa-random");
+let curr_track = document.createElement("audio");
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+  {
+    img: "assets/imgs/who-says.jpg",
+    name: "Who says",
+    artist: "Selena Gomez",
+    description:"Embrace yourself honey!",
+    music: "assets/music/Who-Says.mp3",
+  },
+
+  {
+    img: "assets/imgs/tu-meri.jpeg",
+    name: "Tu meri",
+    artist: "Vishal Shekhar",
+    description:"It takes just your on glance to take my breath away",
+    music: "assets/music/Tu meri.mpeg",
+  },
+
+  {
+    img: "assets/imgs/dil-tu-jaan-tu.jpg",
+    name: "Dil tu Jaan tu",
+    artist: "Gurnazar",
+    description:"My heart , My soul is yours",
+    music: "assets/music/dil-tu-jaan-tu.mp4",
+  },
+
+  {
+    img: "assets/imgs/ve-haaniyan.jpg",
+    name: "Ve Haaniyan",
+    artist: "Avvy Sra, Danny and Sagar",
+    description:"There is no one beside you",
+    music: "assets/music/Ve Haaniyan.mp4",
+  },
+
+  {
+    img: "assets/imgs/baarishein.jpeg",
+    name: "Baarishein",
+    artist: "Anuv Jain",
+    description:"Love is in the rain",
+    music: "assets/music/Baarishein.mp4",
+  },
+
+  {
+    img: "assets/imgs/khwab.jpg",
+    name: "Khwab",
+    artist: "Iqlipse Nova",
+    description:"You are the poem to my poet",
+    music: "assets/music/khwab.mp4",
+  },
+
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  reset();
+
+  curr_track.src = music_list[track_index].music;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+  track_name.textContent = music_list[track_index].name;
+  track_artist.textContent = music_list[track_index].artist;
+  track_desc.textContent = music_list[track_index].description;
+  now_playing.textContent =
+    "Playing music" + (track_index + 1) + " of " + music_list.length;
+  updateTimer = setInterval(setUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+function reset() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+function randomTrack() {
+  isRandom ? pauseRandom() : playRandom();
+}
+
+function playRandom() {
+  isRandom = true;
+  randomIcon.classList.add("randomActive");
+}
+function pauseRandom() {
+  isRandom = false;
+  randomIcon.classList.remove("randomActive");
+}
+function repeatTrack() {
+  let current_index = track_index;
+  loadTrack(current_index);
+  playTrack();
+}
+function playpauseTrack() {
+  isPlaying ? pauseTrack() : playTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  track_art.classList.add("rotate");
+  wave.classList.add("loader");
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  track_art.classList.remove("rotate");
+  wave.classList.remove("loader");
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+
+function nextTrack() {
+  if (track_index < music_list.length - 1 && isRandom === false) {
+    track_index += 1;
+  } else if (track_index < music_list.length - 1 && isRandom === true) {
+    let random_index = Number.parseInt(Math.random() * music_list.length);
+    track_index = random_index;
+  } else {
+    track_index = 0;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0) {
+    track_index -= 1;
+  } else {
+    track_index = music_list.length - 1;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekTo = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekTo;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function setUpdate() {
+  let seekPosition = 0;
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(
+      curr_track.currentTime - currentMinutes * 60
+    );
+
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(
+      (curr_track.duration - durationMinutes * 60)
+    );
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+
+
